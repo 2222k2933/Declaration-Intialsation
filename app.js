0f1761c17527eb5a6164edaf0bd29c8c06ef2fe0
2222k2933 <144654153+2222k2933@users.noreply.github.com>
2024-07-03 20:54:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var petDog="Rex";
+var petCat="Pepper";
+console.log (petDog);
+console.log(petCat);
+console.log("My pet dog name is :"+petDog)
+console.log("My pet cat name is :"+petCat)
+var catSound="purr";
+var dogSound="woof";
+console.log(petDog+"Says"+dogSound);
+console.log(petCat+"Says"+catSound);
+var catSound="meow";
+console.log(catSound);
+console.log(petCat+"now says"+catSound);
